98327b073e89457884c3b0f823fc301375f24659
tenler <tenheller@gmail.com>
2021-01-27 19:49:29 -0500

    Testing 1

---
+++ app/src/main/java/com/hellerkristen/firstapp/MainActivity.java
@@ -0,0 +1,14 @@
+package com.hellerkristen.firstapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
